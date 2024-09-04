22676f2b23a2389615e931ba5e1277f48a96471d
Bartek Paczesny <bartek@paczesny.pl>
2024-09-04 13:43:58 +0200

    fix: container layout not working

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,15 +20,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} min-h-screen`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
           enableSystem
         >
-          <Dashboard>
-            {children}
-          </Dashboard>
+          <div className="grid min-h-screen w-screen md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+            <Sidebar />
+            <div className="flex flex-col flex-1">
+              <Header />
+              <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+                {children}
+              </main>
+            </div>
+          </div>
         </ThemeProvider>
       </body>
     </html>
